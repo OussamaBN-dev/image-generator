5c643b930bb3ef082ee579ebbba346e58c73bd83
OussamaBN-dev <mahammedbenniaoussama@gmail.com>
2026-04-30 01:27:14 +0200

    build Dashboard and WriteArticles pages

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ const navItems = [
 
 const Sidebar = ({sidebar, setSidebar}) => {
     const {user} = useUser();
-    const {signOut, openUserPrifile} = useClerk()
+    const {signOut, openUserProfile} = useClerk()
 
   return (
     <div className={`w-60 bg-white border-r border-gray-200 flex
@@ -29,11 +29,33 @@ const Sidebar = ({sidebar, setSidebar}) => {
         <h1 className="mt-1 text-center">{user.fullName}</h1>
         <div>
             {navItems.map(({to, label, Icon}) =>(
-                <NavLink key={to} tp>
-
+                <NavLink key={to} to={to} end={to === '/ai'} onClick={() =>
+                  setSidebar(false)} className={({isActive}) => `px-3.5 py-2.5 flex 
+                  items-center gap-3 rounded ${isActive ? 'bg-gradient-to-r from-[#3C81F6] to-[#9234EA] text-white': ''}`}>
+                    {({isActive}) => (
+                      <>
+                      <Icon className={`w-4 h-4 ${isActive ? 'text-white' : ''}`} />
+                      {label}
+                      </>
+                    )}
                 </NavLink>
             ))}
         </div>
+      </div>
+
+      <div className="w-full border-t border-gray-200 p-4 px-7 flex items-center justify-between">
+        <div onClick={openUserProfile} className="flex gap-2 items-center cursor-pointer">
+          <img src={user.imageUrl} className="w-8 rounded-full " alt="" />
+          <div>
+            <h1 className="text-sm font-medium">{user.fullName}</h1>
+            <p className="text-xs text-gray-500">
+              <Protect plan='premium' fallback='Free'>Premuim</Protect>Plan 
+            </p>
+          </div>
+        </div>
+          
+        <Logout onClick={signOut} className='w-4.5 text-gray-400 
+        hover:text-gray-700 transition cursor-pointer'/>
       </div>
     </div>
   )
